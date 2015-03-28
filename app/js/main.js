1d1e9cb1cb124608a60ddfc5c516421b69216220
AdituV <aditu.venyhandottir@gmail.com>
2015-03-28 15:03:01 +0000

    Skeleton for showing popover panels in js

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,29 @@
+var loc_panel = null;
+var settings_panel = null;
+var help_panel = null;
 
+var get_user = function () {
+    return null;
+}
+
+var is_logged_in = function (user) {
+    return (user !== null);
+}
+
+var show_panel = function (panel, args) {
+}
+
+var check_login = function (panel) {
+    var user = get_user();
+    if (is_logged_in(user)) {
+        show_panel(panel);
+    } else {
+        show_panel(login, panel);
+    }
+}
+
+$(document).ready(function () {
+    $("#nav_loc").click(function () { check_login(loc_panel) });
+    $("#nav_settings").click(function () { check_login(settings_panel) });
+    $("#nav_help").click(function () { check_login(help_panel) });
+})
