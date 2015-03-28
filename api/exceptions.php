b6255eff94d8224dd0d9fc4d6e97dc9175f1df70
Kay Leacock <kcl@leacock.name>
2015-03-28 16:04:12 +0000

    Added new exceptions for use in authentication classes

--- api/exceptions.php
+++ api/exceptions.php
@@ -4,5 +4,9 @@ namespace TransFlare;
 
 // AuthenticationFailedException: A login, token or other key was invalid.
 class AuthenticationFailedException extends \Exception {};
+// UnableToComplyException: Couldn't honour the request being made.
+class UnableToComplyException extends \Exception {};
+// IncompleteRequestException: A required parameter was not submitted.
+class IncompleteRequestException extends \Exception{};
 
 ?>
