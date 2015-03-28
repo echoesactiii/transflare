bcb526dc96e322dc4b03b8c990c2675dde436d0e
Kay Leacock <kcl@leacock.name>
2015-03-28 14:31:03 +0000

    Initial framework for RedBean & Tonic

--- api/api.php
+++ api/api.php
@@ -2,5 +2,33 @@
 
 require 'include/RedBean/rb.php'; // include RedBean SQL libs
 require 'include/tonic/web/dispatch.php'; // include Tonic REST framework
+require 'config.inc.php'; // include setttings & configuratioon file
+
+class R extends RedBean_Facade {}
+
+R::setup('mysql:host='.$config['sql']['server'].';dbname='.$config['sql']['database'], $config['sql']['username'], $config['sql']['password']);
+
+$app = new Tonic\Application(array(
+	"mount" => array(
+		"TransFlare" => $config['web']['base_url']
+	),
+	"load" => 'api/*.api.php'
+));
+
+$request = new Tonic\Request();
+
+if($request->contentType == 'application/json'){
+	$request->data = json_decode($request->data);
+}
+
+$resource = $app->getResource($request);
+$response = $resource->exec();
+
+// CORS
+$response->accessControlAllowOrigin = "*";
+$response->accessControlAllowMethods = "POST, OPTIONS, GET, PUT, DELETE";
+$response->accessControlAllowHeaders = "X-Token, Content-Type, Content-Length";
+
+$response->output();
 
 ?>
