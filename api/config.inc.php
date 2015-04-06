f64995d232bc0ced029ae34d874a70d9720725c2
Kay Leacock <kcl@leacock.name>
2015-04-06 01:01:19 +0100

    Slim doesn't require base URI

--- api/config.inc.php
+++ api/config.inc.php
@@ -6,9 +6,6 @@ $config['sql']['username'] = "root"; // MySQL Username
 $config['sql']['password'] = "root"; // MySQL Password
 $config['sql']['database'] = "transflare"; // MySQL Database name
 
-// Web Settings
-$config['web']['base_url'] = "/api/"; // Base URL for API requests
-
 // Token encryption settings
 $config['security']['encrypt_key'] = "your.token/encryption!key@here";
 
